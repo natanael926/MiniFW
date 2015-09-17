b173699b9f62e6b7523c4d06f5e6cdd850dd7d0f
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2015-09-17 17:02:40 -0400

    File configuration

---
+++ core/Config/Config.php
@@ -0,0 +1,59 @@
+<?php 
+
+namespace Core\Config;
+
+/**
+ * 
+ * @author Rudys Natanael Acosta Crousset <natanael926@gmail.com>
+ * @version 0.1
+ */
+class Config {
+
+	/**
+	 * @var Config
+	 * @access private
+	 */
+	private static $instance;
+
+	/**
+	 * List of all parameter settings
+	 *  
+	 * @var Config
+	 * @access private
+	 */ 
+	private $rows = [];
+
+	/**
+	 * 
+	 * @return  Config 
+	 */
+	public static function getInstance()
+	{
+
+		if(!isset(self::$instance)) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * @return void
+	 */
+	private function readIniFile()
+	{
+		$this->rows = parse_ini_file("config.ini", true);
+	}
+
+	/**
+	 * @param  $name
+	 * @return string
+	 */
+	public function get($name) 
+	{
+		$this->readIniFile();
+		var_dump($this->rows);
+		return trim($this->rows[$name]);
+	}
+
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('__ROOT__', dirname(dirname(__FILE__))); 
+
+require_once(__ROOT__ . '/MiniFW/core/Config/Config.php');
+
+// use Core\Config\Config; 
+
+// var_dump(Config::getInstance()->get('host'));
